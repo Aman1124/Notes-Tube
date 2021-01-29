1807efb34857168a00aed496309ab107cf87218a
Aman1124 <amankumarthakur666@gmail.com>
2021-01-30 01:21:08 +0530

    Added login and register UI

---
+++ app/src/main/java/com/example/notestube/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.example.notestube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button button = findViewById(R.id.regSignInButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Intent intent = getIntent();
+    }
+}
